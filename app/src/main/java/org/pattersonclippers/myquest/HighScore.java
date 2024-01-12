66be94df98fee44d08e6996a92dbbd5bb03f1617
JessieHolley <jessieholley12@gmail.com>
2024-01-12 10:48:15 -0500

    Added firebase

--- app/src/main/java/org/pattersonclippers/myquest/HighScore.java
+++ app/src/main/java/org/pattersonclippers/myquest/HighScore.java
@@ -1,4 +1,36 @@
 package org.pattersonclippers.myquest;
 
 public class HighScore {
+    private int finalScore;
+    private String name;
+
+    public HighScore(int finalScore, String name) {
+        this.finalScore = finalScore;
+        this.name = name;
+    }
+
+
+    public int getFinalScore() {
+        return finalScore;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setFinalScore(int finalScore) {
+        this.finalScore = finalScore;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "HighScore{" +
+                "finalScore=" + finalScore +
+                ", name='" + name + '\'' +
+                '}';
+    }
 }
--- app/src/main/java/org/pattersonclippers/myquest/ScoreActivity.java
+++ app/src/main/java/org/pattersonclippers/myquest/ScoreActivity.java
@@ -4,10 +4,32 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class ScoreActivity extends AppCompatActivity {
     TextView scoreFTV;
+
+    EditText nameET;
+
+    Button highBT;
+
+    FirebaseDatabase database;
+
+    DatabaseReference myRef;
+
+    HighScore myHS;
+
+    String myName;
+
+    Intent myIntent;
+
+    int score;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,10 +37,24 @@ public class ScoreActivity extends AppCompatActivity {
         setContentView(R.layout.activity_score);
 
         scoreFTV = (TextView) findViewById(R.id.scoreF);
+        nameET = (EditText) findViewById(R.id.NameET);
+        highBT = (Button) findViewById(R.id.HighBT);
+        database = FirebaseDatabase.getInstance();
+        myRef = database.getReference("My score");
 
 
-        Intent myIntent = getIntent();
-        int score = myIntent.getIntExtra("score", 0);
+        myIntent = getIntent();
+        score = myIntent.getIntExtra("score", 0);
         scoreFTV.setText(""+score);
+
+
+        highBT.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                myName = nameET.getText().toString();
+                myHS = new HighScore(score, myName);
+                myRef.setValue(myHS);
+            }
+        });
     }
 }
